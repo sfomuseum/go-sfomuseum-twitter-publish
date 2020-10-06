e58f195c39ad78bcbf7566d6fb566bf2ad0891ef
thisisaaronland <devnull@localhost>
2020-10-06 15:10:40 -0700

    start wiring in WOF stuff

--- go.mod
+++ go.mod
@@ -4,5 +4,10 @@ go 1.12
 
 require (
 	github.com/sfomuseum/go-sfomuseum-twitter v0.0.3
+	github.com/tidwall/gjson v1.6.1
+	github.com/tidwall/sjson v1.1.2
+	github.com/whosonfirst/go-reader v0.2.0
+	github.com/whosonfirst/go-whosonfirst-uri v0.1.0
+	github.com/whosonfirst/go-writer v0.2.0
 	gocloud.dev v0.20.0
 )
--- publish.go
+++ publish.go
@@ -2,13 +2,25 @@ package publish
 
 import (
 	"context"
+	"errors"
+	"io/ioutil"
 	"log"
+	"github.com/sfomuseum/go-sfomuseum-twitter/document"
+	"github.com/whosonfirst/go-reader"
+	"github.com/whosonfirst/go-writer"
+	"github.com/whosonfirst/go-whosonfirst-uri"		
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+	"sync"
 )
 
 type PublishOptions struct {
+	Lookup *sync.Map
+	Reader reader.Reader
+	Writer writer.Writer
 }
 
-func PublishTweet(ctx context.Context, opts *PublishOptions, tweet []byte) error {
+func PublishTweet(ctx context.Context, opts *PublishOptions, body []byte) error {
 
 	select {
 	case <-ctx.Done():
@@ -17,6 +29,71 @@ func PublishTweet(ctx context.Context, opts *PublishOptions, tweet []byte) error
 		// pass
 	}
 
-	log.Println(string(tweet))
+	id_rsp := gjson.GetBytes(body, "id")
+
+	if !id_rsp.Exists() {
+		return errors.New("Missing 'id' property")
+	}
+
+	tweet_id := id_rsp.String()
+
+	tweet_body, err := document.AppendCreatedAtTimestamp(ctx, body)
+
+	if err != nil {
+		return nil
+	}
+	
+	pointer, ok := opts.Lookup.Load(tweet_id)
+
+	var wof_record []byte
+	
+	if ok {
+		
+		wof_id := pointer.(int64)
+		rel_path, err := uri.Id2RelPath(wof_id)
+
+		if err != nil {
+			return err
+		}
+		
+		wof_fh, err := opts.Reader.Read(ctx, rel_path)
+
+		if err != nil {
+			return err
+		}
+
+		defer wof_fh.Close()
+
+		wof_body, err := ioutil.ReadAll(wof_fh)
+
+		if err != nil {
+			return err
+		}
+
+		wof_record = wof_body
+		
+	} else {
+
+		new_record, err := newWOFRecord(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		wof_record = new_record
+	}
+
+	wof_record, err = sjson.SetBytes(wof_record, "properties.concordances.twitter:id", tweet_id)
+
+	if err != nil {
+		return err
+	}
+		
+	log.Println(string(tweet_body))
 	return nil
+}
+
+func newWOFRecord(ctx context.Context) ([]byte, error){
+
+	return nil, errors.New("Not implemented")
 }
