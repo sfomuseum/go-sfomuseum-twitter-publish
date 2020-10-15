304b0520900adeef5a06759b2b7b11b73fd63e55
thisisaaronland <devnull@localhost>
2020-10-14 17:31:04 -0700

    fix repo name

--- cmd/publish/main.go
+++ cmd/publish/main.go
@@ -21,7 +21,7 @@ func main() {
 	trim_prefix := flag.Bool("trim-prefix", true, "Trim default tweet.js JavaScript prefix.")
 
 	indexer_uri := flag.String("indexer-uri", "git://", "A valid whosonfirst/go-whosonfirst-index URI")
-	indexer_path := flag.String("indexer-path", "git@github.com:sfomuseum-data/sfomuseum-data-twitter.git", "...")
+	indexer_path := flag.String("indexer-path", "git@github.com:sfomuseum-data/sfomuseum-data-socialmedia-twitter.git", "...")
 
 	reader_uri := flag.String("reader-uri", "", "A valid whosonfirst/go-reader URI")
 	writer_uri := flag.String("writer-uri", "", "A valid whosonfirst/go-writer URI")
