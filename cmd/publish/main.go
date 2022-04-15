9014bb9b922c4eec816e3f62462499474269cd0e
thisisaaronland <thisisaaronland@localhost>
2022-04-15 16:24:53 -0700

    Update defaults

--- cmd/publish/main.go
+++ cmd/publish/main.go
@@ -21,8 +21,8 @@ func main() {
 
 	iterator_uri := flag.String("iterator-uri", "repo://", "A valid whosonfirst/go-whosonfirst-iterate/v2 URI")
 
-	reader_uri := flag.String("reader-uri", "", "A valid whosonfirst/go-reader URI")
-	writer_uri := flag.String("writer-uri", "", "A valid whosonfirst/go-writer URI")
+	reader_uri := flag.String("reader-uri", "fs:///usr/local/data/sfomuseum-data-socialmedia-twitter/data", "A valid whosonfirst/go-reader URI")
+	writer_uri := flag.String("writer-uri", "fs:///usr/local/data/sfomuseum-data-socialmedia-twitter/data", "A valid whosonfirst/go-writer URI")
 
 	flag.Parse()
 
