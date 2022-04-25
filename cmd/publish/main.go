c8103fe7774592051735c556061d3a6963df91f4
thisisaaronland <aaron@localhost>
2022-04-25 10:44:37 -0700

    update vendor deps

--- cmd/publish/main.go
+++ cmd/publish/main.go
@@ -20,7 +20,7 @@ func main() {
 	trim_prefix := flag.Bool("trim-prefix", true, "Trim default tweet.js JavaScript prefix.")
 
 	iterator_uri := flag.String("iterator-uri", "repo://", "A valid whosonfirst/go-whosonfirst-iterate/v2 URI")
-	iterator_source := flag.String("iterator-source", "/usr/local/data/sfomuseum-data-socialmedia-instagram/data", "...")
+	iterator_source := flag.String("iterator-source", "/usr/local/data/sfomuseum-data-socialmedia-twitter", "...")
 
 	reader_uri := flag.String("reader-uri", "fs:///usr/local/data/sfomuseum-data-socialmedia-twitter/data", "A valid whosonfirst/go-reader URI")
 	writer_uri := flag.String("writer-uri", "fs:///usr/local/data/sfomuseum-data-socialmedia-twitter/data", "A valid whosonfirst/go-writer URI")
